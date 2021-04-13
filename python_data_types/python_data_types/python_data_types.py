cc79eeafef9f063044c946e938973bdc54dcacf9
lane froehlich <elaine.froehlich@gmail.com>
2021-04-12 20:22:57 -0400

    The read input files are here.

---
+++ python_data_types/python_data_types/python_data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+num = int(input())
+doub = float(input())
+text = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + num)
+
+# Print the sum of the double variables on a new line.
+print(d + doub)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + text)
