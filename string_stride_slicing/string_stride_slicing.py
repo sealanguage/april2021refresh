cb5e55439357adc772e2519845d45096f9956ab9
lane froehlich <elaine.froehlich@gmail.com>
2021-05-02 20:25:09 -0400

    have hard code to stride the strings, starting to iterate input strings

--- string_stride_slicing/string_stride_slicing.py
+++ string_stride_slicing/string_stride_slicing.py
@@ -6,8 +6,7 @@ rank = input()
 
 # print(s, word, rank)  # prints 2 Hacker Rank
 
-# number_string = "1020304050"
-# print(number_string[0:-1:2])
+print(word[0:len(word):2], word[1:len(word):2])  # splits into Hcr ake
+print(rank[0:4:2], rank[1:4:2])  # splits into Ra nk
 
-print(word[0:7:2], word[1:7:2])
-print(rank[0:4:2], rank[1:4:2])
+# now need to iterate through the input and replace the middle number with len()
