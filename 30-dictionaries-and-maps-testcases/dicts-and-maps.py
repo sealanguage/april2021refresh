68cac5a5b4ce6c7269f05b0d2b95a77fb659a779
lane froehlich <elaine.froehlich@gmail.com>
2021-05-03 21:14:32 -0400

    Got help with ways to create dictionaries and working on stride slicing in python

--- 30-dictionaries-and-maps-testcases/dicts-and-maps.py
+++ 30-dictionaries-and-maps-testcases/dicts-and-maps.py
@@ -1,4 +1,6 @@
-d = {}
+d = {'string': 1}
+
+#  'sam' : 2223456, 'edward' : 1234321,
 
 with open('input00.txt') as f:
     lines = f.readlines()
@@ -26,3 +28,20 @@ for line in open('input00.txt'):
     # for row in lines:
     #     listitems = lines.split()
     #     print('listitmes: ', listitems)
+
+
+my_dict = {"Name":[],"Address":[],"Age":[]};
+
+my_dict["Name"].append("Guru")
+my_dict["Address"].append("Mumbai")
+my_dict["Age"].append(30)
+print(my_dict)
+print(my_dict.values())
+print(my_dict.keys())
+print(my_dict.get("Age"))
+print(my_dict.get(30))  # answer is none
+print(my_dict.fromkeys('Name'))
+
+x = ('key1', 'key2')
+y = 0
+print()
--- string_stride_slicing/string_stride_slicing.py
+++ string_stride_slicing/string_stride_slicing.py
@@ -1,12 +1,35 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-s = int(input())
-word = input()
-rank = input()
+# s = int(input())
+# word = input()
+# rank = input()
 
 # print(s, word, rank)  # prints 2 Hacker Rank
 
-print(word[0:len(word):2], word[1:len(word):2])  # splits into Hcr ake
-print(rank[0:4:2], rank[1:4:2])  # splits into Ra nk
+# print("hard coded: ", word[0:len(word):2], word[1:len(word):2])  # WORKS splits into Hcr ake
+# print("hard coded: ", rank[0:4:2], rank[1:4:2])  # WORKS splits into Ra nk
 
 # now need to iterate through the input and replace the middle number with len()
+
+if __name__ == '__main__':
+    s = input()
+    first_word = input()
+    second_word = input()
+
+    print(first_word[0:len(first_word):2], first_word[1:len(first_word):2])
+    print(second_word[0:len(second_word):2], second_word[1:len(second_word):2])
+
+# for stringy in input():  # prints o and 1
+
+#         print(stringy)
+
+# for letter in word:
+#     letter[0:7:2]
+#     print(letter)
+
+# for i in range(n):
+#     a, b = input().strip().split(' ')
+#     print (a, b)
+
+# hack = list(word)
+# print(hack)
