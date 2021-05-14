f6b26419b658d3e00630dc04ef70b0b251e576bd
lane froehlich <elaine.froehlich@gmail.com>
2021-05-13 21:07:09 -0400

    code adds all numbers in input k times, does not recurse

--- digit_sum/digit_sum.py
+++ digit_sum/digit_sum.py
@@ -22,14 +22,51 @@ def superDigit(n, k):
     one = 1
     sum = 0
     res = [int(n[idx: idx + one]) for idx in range(0, len(n), one)]
+    holder = []
+    holderStack = []
+    count = 0
 
-    for i in res:
-        sum = sum + i
+    # def addToSum():
 
-    print(sum)
+    #  This works to add the numbers in the initial res
+    for count in range(k):  # count is 0 0 0 1 1 1 2 2 2
+        for i in res:
+            sum = sum + i
+            print("count ", count)
 
-    print(k, res)
-    return
+            # if sum < 10:
+            #     print(sum)
+            # else:
+            #     sum = res = sum
+        print(sum)
+        print('res ', res)
+
+        # holderStack.append(i)
+        # print("hS ", holderStack)
+        # return sum
+    # addToSum()
+
+    #     if sum < 10:
+    #         print("sum less 10 ", sum)
+    # else:
+    #     for n in holderStack:
+    #         sum = sum + n
+
+    # print("sum = sum + n; ", sum)
+
+    # print("hS2 ", holderStack)
+
+    # print(k, res)
+
+    # for val in range(sum):
+    #     holderStack.append(val)
+    # print(holderStack)
+    # for i in range(sum):
+    #     holder.append(i)
+    # check = len(str(sum))
+    # if check > 1:
+    #     return
+    # write this as a function then call the function again here till check length is 1
 
     #  iterate through res ad add each number together
 
